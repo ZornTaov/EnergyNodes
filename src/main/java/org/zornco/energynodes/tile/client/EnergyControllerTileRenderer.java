549dbf97d254f9eae91047f5872fc2c148297005
ZornTaov <zeke_kjell2@yahoo.com>
2022-10-13 06:34:54 -0700

    initial attempt to render fluids to the screen, and it all goes wrong at first

--- src/main/java/org/zornco/energynodes/tile/client/EnergyControllerTileRenderer.java
+++ src/main/java/org/zornco/energynodes/tile/client/EnergyControllerTileRenderer.java
@@ -44,6 +44,45 @@ public class EnergyControllerTileRenderer extends BaseControllerTileRenderer<Ene
     @Override
     public void renderAdditional(EnergyControllerTile te, PoseStack matrixStack, MultiBufferSource buffer, Font fontrenderer, int overlay, int light) {
 
+        //render fluid
+        matrixStack.pushPose();
+        FluidStack fluidStack = new FluidStack(Fluids.FLOWING_LAVA, 1000);
+        IClientFluidTypeExtensions fluidTypeExtensions = IClientFluidTypeExtensions.of(fluidStack.getFluid());
+        ResourceLocation stillTex = fluidTypeExtensions.getStillTexture();
+        TextureAtlasSprite sprite = Minecraft.getInstance().getTextureAtlas(TextureAtlas.LOCATION_BLOCKS).apply(stillTex);
+
+        int color = 0x77FFFFFF;//fluidTypeExtensions.getTintColor(fluidStack);
+
+        int[] col = new int[] {
+            (color >> 16) & 0xFF,
+            (color >> 8)  & 0xFF,
+            color        & 0xFF,
+            (color >> 24) & 0xFF
+        };
+        VertexConsumer builder = buffer.getBuffer(Sheets.translucentCullBlockSheet());
+        matrixStack.translate(0.0F, 0.0, -0.01);
+
+        Matrix4f matrix = matrixStack.last().pose();
+        Matrix3f normal = matrixStack.last().normal();
+        //EnergyNodes.LOGGER.info(i + ", " + i1);
+        float minX = -0.5f;
+        float maxX = 0.5f;
+        float minY = 0.5f;
+        float maxY = 1.5f;
+        float ny = 1;
+
+        float minU = sprite.getU(0);
+        float maxU = sprite.getU(16);
+        float minV = sprite.getV(0);
+        float maxV = sprite.getV(16);
+
+        builder.vertex(matrix, minX, minY, 0).color(col[0], col[1], col[2], col[3]).uv(maxU, minV).overlayCoords(overlay).uv2(light).normal(normal, 0, ny, 0).endVertex();
+        builder.vertex(matrix, maxX, minY, 0).color(col[0], col[1], col[2], col[3]).uv(minU, minV).overlayCoords(overlay).uv2(light).normal(normal, 0, ny, 0).endVertex();
+        builder.vertex(matrix, maxX, maxY, 0).color(col[0], col[1], col[2], col[3]).uv(minU, maxV).overlayCoords(overlay).uv2(light).normal(normal, 0, ny, 0).endVertex();
+        builder.vertex(matrix, minX, maxY, 0).color(col[0], col[1], col[2], col[3]).uv(maxU, maxV).overlayCoords(overlay).uv2(light).normal(normal, 0, ny, 0).endVertex();
+
+        matrixStack.popPose();
+
         //render text
         matrixStack.pushPose();
         matrixStack.translate(0.10F, -0.25F, 0.0F);
